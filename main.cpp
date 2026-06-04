7e49008183241613f0647bf70ea56d5c2b650f7a
MarekSlusarczyk <114940674+MarekSlusarczyk@users.noreply.github.com>
2026-06-04 20:20:35 +0200

    txt file handling + time measurement

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "exactTSPSolver.h"
 #include "heuristicTSPSolver.h"
 #include "myexceptions.h"
+#include <chrono>
+#include <fstream>
 
 using namespace std;
 
@@ -10,29 +12,60 @@ using namespace std;
 int main() {
     try { // Blok przechwytujacy wyjatki rzucane z klas
         
-        // Polimorfizm: Uzywamy wskaznika na interfejs bazowy, ale tworzymy obiekt klasy pochodnej
-        DistanceGraph* g = new DistanceGraphExt(6); 
+        //// Polimorfizm: Uzywamy wskaznika na interfejs bazowy, ale tworzymy obiekt klasy pochodnej
+        //DistanceGraph* g = new DistanceGraphExt(6); 
 
-        
-        // Zapisywanie polaczen w grafie (mapie)
-        g->setDistance(0, 1, 14); 
-        g->setDistance(0, 2, 3);  
-        g->setDistance(0, 3, 20); 
-        g->setDistance(0, 4, 11);
-        g->setDistance(0, 5, 2);
-        g->setDistance(1, 2, 1); 
-        g->setDistance(1, 3, 6); 
-        g->setDistance(1, 4, 17);  
-        g->setDistance(1, 5, 19);  
-        g->setDistance(2, 3, 16);  
-        g->setDistance(2, 4, 3);
-        g->setDistance(2, 5, 10); 
-        g->setDistance(3, 4, 12); 
-        g->setDistance(3, 5, 4);  
-        g->setDistance(4, 5, 5);  
+        //
+        //// Zapisywanie polaczen w grafie (mapie)
+        //g->setDistance(0, 1, 14); 
+        //g->setDistance(0, 2, 3);  
+        //g->setDistance(0, 3, 20); 
+        //g->setDistance(0, 4, 11);
+        //g->setDistance(0, 5, 2);
+        //g->setDistance(1, 2, 1); 
+        //g->setDistance(1, 3, 6); 
+        //g->setDistance(1, 4, 17);  
+        //g->setDistance(1, 5, 19);  
+        //g->setDistance(2, 3, 16);  
+        //g->setDistance(2, 4, 3);
+        //g->setDistance(2, 5, 10); 
+        //g->setDistance(3, 4, 12); 
+        //g->setDistance(3, 5, 4);  
+        //g->setDistance(4, 5, 5);  
 
-        cout << "Macierz odleglosci:\n";
-        cout << *g << "\n"; // Uzycie przeciazonego operatora <<
+        //cout << "Macierz odleglosci:\n";
+        //cout << *g << "\n"; // Uzycie przeciazonego operatora <<
+
+        // Otwieramy plik tekstowy z danymi grafu, gdzie pierwsza linia to liczba miast
+        ifstream file("graf.txt");
+        if (!file.is_open()) {
+            throw MyExceptions("Blad: Nie udalo sie otworzyc pliku graf.txt");
+        }
+
+        // Wczytujemy liczbê miast (pierwsza wartoœæ z pliku)
+        unsigned n;
+        if (!(file >> n)) {
+            throw MyExceptions("Blad: Brak liczby miast");
+        }
+
+        // Tworzymy obiekt grafu i
+        // przekazujemy dynamicznie zmienn¹ 'n' wczytan¹ z pliku
+        DistanceGraph* g = new DistanceGraphExt(n);
+
+        // 4. Za pomoc¹ pêtli wczytujemy resztê macierzy i uzupe³niamy graf
+        for (unsigned i = 0; i < n; i++) {
+            for (unsigned j = 0; j < n; j++) {
+                unsigned val;
+                if (!(file >> val)) {
+                    throw MyExceptions("Blad: Za malo danych w pliku grafu!");
+                }
+                g->setDistance(i, j, val); // U¿ywamy standardowej metody z instrukcji
+            }
+        }
+        file.close(); // Zamykamy strumieñ pliku, dane s¹ ju¿ bezpieczne w obiekcie grafu
+
+        cout << "Macierz odleglosci zaladowana z pliku (Rozmiar: " << n << "):\n";
+        cout << *g << "\n";
 
         /*Tworzenie "ekspertow" do liczenia tras.
         Tu rowniez widac polimorfizm - pod jednym interfejsem TSPSolver kryja sie rozne algorytmy.*/
@@ -41,22 +74,39 @@ int main() {
         TSPSolver* greedySE = new HeuristicTSPSolver(1);
 
         // Wyznaczanie tras
+        // Pomiar dla algorytmu dok³adnego
+        auto startExact = chrono::high_resolution_clock::now();
         Tour<unsigned> t1 = exact->solve(*g, 0);
+        auto endExact = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> durationExact = endExact - startExact;
+
+        // Pomiar dla heurystyki Najbli¿szego S¹siada (NN)
+        auto startNN = chrono::high_resolution_clock::now();
         Tour<unsigned> t2 = greedyNN->solve(*g, 0);
+        auto endNN = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> durationNN = endNN - startNN;
+
+        // Pomiar dla heurystyki Najmniejszej Krawêdzi (SE)
+        auto startSE = chrono::high_resolution_clock::now();
         Tour<unsigned> t3 = greedySE->solve(*g, 0);
+        auto endSE = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> durationSE = endSE - startSE;
 
         // Wyswietlanie wynikow 
         cout << "=== ALGORYTM DOKLADNY ===\n";
         cout << t1 << "\n"; // Operator << dla trasy Tour
-        cout << "Koszt: " << t1.totalCost(*g) << "\n\n";
+        cout << "Koszt: " << t1.totalCost(*g) << "\n";
+        cout << "Czas wykonania: " << durationExact.count() << " ms\n\n";
 
         cout << "=== HEURYSTYKA NN ===\n";
         cout << t2 << "\n";
-        cout << "Koszt: " << t2.totalCost(*g) << "\n\n";
+        cout << "Koszt: " << t2.totalCost(*g) << "\n";
+        cout << "Czas wykonania: " << durationNN.count() << " ms\n\n";
 
         cout << "=== HEURYSTYKA SMALLEST EDGE ===\n";
         cout << t3 << "\n";
-        cout << "Koszt: " << t3.totalCost(*g) << "\n\n";
+        cout << "Koszt: " << t3.totalCost(*g) << "\n";
+        cout << "Czas wykonania: " << durationSE.count() << " ms\n\n";
 
         // Zarzadzanie pamiecia - instrukcja delete zwalnia wczesniej przydzielona nowa pamiec (new)
         delete greedySE;
